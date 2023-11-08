0629e71169e0202ec05a6257de11e1514e97b0f3
Lucas Esclapez <l.esclapez@esciencecenter.nl>
2023-11-08 14:15:59 +0100

    Add bundled domain params.

--- popomo/popomo.py
+++ popomo/popomo.py
@@ -1,6 +1,9 @@
 from pytams.fmodel import ForwardModel
 
 from popomo.poputils import getPOPinstance
+from popomo.poputils import plot_depth
+
+from omuse.units import units
 
 
 class POPOmuseModel(ForwardModel):
@@ -12,16 +15,25 @@ class POPOmuseModel(ForwardModel):
         self._nml_file = params.get("nml_file", "pop_in")
         self._topo_file = params.get("topo_file", None)
         self._nProc_pop = params.get("nProc_POP", 1)
+        self._popDomain = {"lonMin" : params.get("lonmin", -180) | units.deg,
+                           "lonMax" : params.get("lonmax",  180) | units.deg,
+                           "latMin" : params.get("latmin", -84) | units.deg,
+                           "latMax" : params.get("latmax",  84) | units.deg,
+                           "Nx" : params.get("Nx", 120),
+                           "Ny" : params.get("Ny", 56),
+                           "Nz" : params.get("Nz", 12)}
 
     def advance(self, dt: float, forcingAmpl: float):
         """Override the template."""
         p = getPOPinstance(
-            nworkers=self._nProc_pop,
-            nml_file=self._nml_file,
-            topo_file=self._topo_file,
+            nml_file = self._nml_file,
+            nworkers = self._nProc_pop,
+            topo_file = self._topo_file,
+            domain_dict = self._popDomain,
         )
         tnow = p.model_time
         print(tnow)
+        plot_depth(p, "depth.png")
 
     def getCurState(self):
         """Override the template."""
