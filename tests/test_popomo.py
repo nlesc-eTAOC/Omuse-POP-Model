2e0c8f73f82d77cc90b05bb7d8c5415e4b417d49
Lucas Esclapez <l.esclapez@esciencecenter.nl>
2023-11-15 11:24:09 +0100

    Update test.

--- tests/test_popomo.py
+++ tests/test_popomo.py
@@ -14,12 +14,26 @@ def test_initModel():
     assert fmodel.name() == "POPOmuseModel"
 
 
-def test_advanceModel():
+def test_advance3degModel():
     """Advance an POP-Omuse model over a step."""
     pop_params = {
         "nml_file": "./data/pop_in",
         "topo_file": "./data/KMT_120_56.csv",
         "nProc_POP": 1,
+    }
+    fmodel = POPOmuseModel(params=pop_params)
+    fmodel.advance(0.1, 0.1)
+
+
+def test_advance1degModel():
+    """Advance an POP-Omuse model over a step."""
+    pop_params = {
+        "nml_file": "./data/pop_in",
+        "topo_file": "./data/KMT_360_168.csv",
+        "nProc_POP": 1,
+        "Nx": 360,
+        "Ny": 168,
+        "Nz": 24,
     }
     fmodel = POPOmuseModel(params=pop_params)
     fmodel.advance(0.1, 0.1)
