82e6ecf9978cc5c0ec94aa9f9d81746d5965b30a
Lucas Esclapez <l.esclapez@esciencecenter.nl>
2024-04-16 11:29:09 +0200

    Update POP utility functions.

--- popomo/poputils.py
+++ popomo/poputils.py
@@ -1,3 +1,5 @@
+import os
+import shutil
 import matplotlib
 import numpy as np
 from matplotlib import pyplot
@@ -11,53 +13,6 @@ class POPUtilsError(Exception):
     """Exception class for POPUtils."""
 
     pass
-
-
-def plot_globe(p, value, unit, name, elements=False):
-    """Plot value of the globe."""
-    matplotlib.use("agg")
-    mask = p.elements.depth.value_in(units.km) == 0
-    value = np.ma.array(value, mask=mask)
-
-    if elements:
-        x = p.elements3d.lon[:, 0, 0].value_in(units.deg)
-        y = p.elements3d.lat[0, :, 0].value_in(units.deg)
-    else:
-        x = p.nodes3d.lon[:, 0, 0].value_in(units.deg)
-        y = p.nodes3d.lat[0, :, 0].value_in(units.deg)
-
-    for i in range(len(x)):
-        if x[i] > 180:
-            x[i] = x[i] - 360
-
-    i = np.argsort(x)
-    i = np.insert(i, 0, i[-1])
-
-    value = value[i, :]
-    x = x[i]
-    x[0] -= 360
-
-    pyplot.figure(figsize=(7, 3.5))
-
-    pyplot.contourf(x, y, value.T)
-
-    pyplot.xticks(
-        [-180, -120, -60, 0, 60, 120, 180],
-        ["180°W", "120°W", "60°W", "0°", "60°E", "120°E", "180°E"],
-    )
-    pyplot.yticks(
-        [-60, -30, 0, 30, 60], ["60°S", "30°S", "0°", "30°N", "60°N"]
-    )
-    pyplot.colorbar(label=unit)
-    pyplot.ylim(y[1], y[-2])
-    pyplot.savefig(name)
-    pyplot.close()
-
-
-def plot_depth(p, name="depth.png"):
-    """Plot the ocean depth level."""
-    h = p.nodes.depth.value_in(units.km)
-    plot_globe(p, h, "km", name)
 
 
 def depth_levels(N: int, stretch_factor: float = 1.8) -> np.ndarray:
@@ -78,32 +33,47 @@ def depth_levels(N: int, stretch_factor: float = 1.8) -> np.ndarray:
 
 
 def getPOPinstance(
-    nworkers: int = 1,
-    nml_file: str = "./pop_in",
-    domain_dict: dict = None,
+    pop_domain_dict: dict = None,
+    pop_options: dict = None,
+    root_run_folder: str = None
 ) -> POP:
     """Return an instance of POP."""
-    assert domain_dict is not None
+    assert pop_domain_dict is not None
 
-    # Mode is based on resolution
+    # Mode and worker name are based on resolution
     mode = "{}x{}x{}".format(
-        domain_dict["Nx"], domain_dict["Ny"], domain_dict["Nz"]
+        pop_domain_dict["Nx"], pop_domain_dict["Ny"], pop_domain_dict["Nz"]
     )
+
+    # Set POP output redirection
+    redirect = pop_options.get("redirect", "none")
+    redirect_file = "pop_log.txt"
+    if root_run_folder:
+        redirect_file = "{}/pop_log.txt".format(root_run_folder)
+
+    # Copy the nml option file to the run folder
+    orig_nml_file = pop_options.get("nml_file", "pop_in")
+    target_nml_file = "{}/{}".format(root_run_folder, os.path.basename(orig_nml_file))
+    shutil.copy(orig_nml_file, target_nml_file)
+
+    # Instantiate POP
     p = POP(
-        number_of_workers=nworkers,
+        number_of_workers=pop_options.get("nProc",1),
         mode=mode,
-        namelist_file=nml_file,
-        redirection="none",
+        namelist_file=target_nml_file,
+        redirection=redirect,
+        redirect_file=redirect_file,
+        channel_type=pop_options.get("channel", "mpi")
     )
 
     # Grid option: either amuse or pop_files
-    grid_option = domain_dict.get("grid_option", "amuse")
+    grid_option = pop_domain_dict.get("grid_option", "amuse")
 
     if grid_option == "amuse":
-        # Prepare grid data
-        levels = depth_levels(domain_dict["Nz"] + 1) * 5000 | units.m
+        # Amuse generate a cartesian lat/long grid
+        levels = depth_levels(pop_domain_dict["Nz"] + 1) * 5000 | units.m
         depth_in = np.loadtxt(
-            domain_dict["topography_file"], delimiter=",", dtype=int
+            pop_domain_dict["topography_file"], delimiter=",", dtype=int
         )
         dz = levels[1:] - levels[:-1]
 
@@ -113,18 +83,19 @@ def getPOPinstance(
         p.parameters.vertical_layer_thicknesses = dz
 
         p.parameters.horiz_grid_option = "amuse"
-        p.parameters.lonmin = domain_dict.get("lonmin", -180) | units.deg
-        p.parameters.lonmax = domain_dict.get("lonmax", 180) | units.deg
-        p.parameters.latmin = domain_dict.get("latmin", -84) | units.deg
-        p.parameters.latmax = domain_dict.get("latmax", 84) | units.deg
+        p.parameters.lonmin = pop_domain_dict.get("lonmin", -180) | units.deg
+        p.parameters.lonmax = pop_domain_dict.get("lonmax", 180) | units.deg
+        p.parameters.latmin = pop_domain_dict.get("latmin", -84) | units.deg
+        p.parameters.latmax = pop_domain_dict.get("latmax", 84) | units.deg
 
     elif grid_option == "pop_files":
+        # Standard pop file use fancy grid
         p.parameters.topography_option = "file"
-        p.parameters.topography_file = domain_dict.get("topography_file", None)
+        p.parameters.topography_file = pop_domain_dict.get("topography_file", None)
         p.parameters.vert_grid_option = "file"
-        p.parameters.vert_grid_file = domain_dict.get("vert_grid_file", None)
+        p.parameters.vert_grid_file = pop_domain_dict.get("vert_grid_file", None)
         p.parameters.horiz_grid_option = "file"
-        p.parameters.horiz_grid_file = domain_dict.get("horiz_grid_file", None)
+        p.parameters.horiz_grid_file = pop_domain_dict.get("horiz_grid_file", None)
     else:
         raise POPUtilsError(
             "Unknown grid_option {}. Either 'amuse' or 'pop_files'".format(
@@ -135,17 +106,29 @@ def getPOPinstance(
     return p
 
 
+def setStoichForcingAmpl(p: POP, a_ampl: float):
+    """Set the FWF stoichastic amplitude."""
+    print("Stoich. amplitude is: {}".format(a_ampl))
+    p.parameters.stoich_ampl = a_ampl
+
+
 def setCheckPoint(p: POP, freq: int, chkprefix: str):
     """Set the POP instance checkpointing options."""
     # Set restart such that a single restart is called at
     # the end of the advance function.
-    p.parameters.restart_option = "nhour"
-    p.parameters.restart_freq_option = freq
+    p.parameters.restart_option = "nmonth"
+    p.parameters.restart_freq_option = 1
     p.parameters.restart_file = chkprefix
+
+
+def disableCheckPoint(p: POP):
+    """Disable restart file IO in POP."""
+    p.parameters.restart_option = "never"
 
 
 def setRestart(p: POP, rstFile: str):
     p.parameters.ts_file = rstFile
+    p.parameters.ts_file_format = "bin"
 
 
 def getLastRestart(p: POP) -> str:
