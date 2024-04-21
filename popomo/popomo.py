1f725a0f9eb53753d73c18c50c41cb5339ab57aa
Lucas Esclapez <l.esclapez@esciencecenter.nl>
2024-04-21 22:20:46 +0200

    Update for .toml based input.

--- popomo/popomo.py
+++ popomo/popomo.py
@@ -1,72 +1,121 @@
 import os
+import time
 
 import numpy as np
 from omuse.units import units
 from pytams.fmodel import ForwardModel
+import psutil as psu
 
 from popomo.poputils import (
     getLastRestart,
     getPOPinstance,
     setCheckPoint,
+    disableCheckPoint,
     setRestart,
+    setStoichForcingAmpl
+)
+from popomo.utils import (
+    remainingdaysincurrentmonth,
+    daysincurrentyear,
+    monthly_reference,
 )
 
 
 class POPOmuseModel(ForwardModel):
     """A forward model for pyTAMS based on POP-Omuse."""
 
-    def __init__(self, params: dict = None, ioprefix: str = None) -> None:
+    def __init__(self,
+                 params: dict = None,
+                 ioprefix: str = None) -> None:
         """Override the template."""
+        # Stash away POP-specific parameters
+        self._pop_params = params.get("pop", {})
+
         # In this model, the state is a pointer to a POP restart file
+        # Upon initialization, pass a initial solution if one is provided
         self._state = None
-        self._nml_file = params.get("nml_file", "pop_in")
-        self._nProc_pop = params.get("nProc_POP", 1)
-        self._popDomain = params.get("domain_dict", None)
+        init_file = self._pop_params.get("init_file", None)
+        if (init_file):
+            assert os.path.exists(init_file) is True
+            self._state = init_file
+
+        # POP simulation is not starting at t=0
+        # keep track of initial time
+        self._init_time = self._pop_params.get("init_time", 0.0)
+
+        # POP domain parameters
+        self._popDomain = self._pop_params.get("domain_dict", None)
         assert self._popDomain is not None
 
+        # The actual POP object
         self.pop = None
+
+        # Database information
         self.checkpoint_prefix = None
-        if params.get("DB_save", False):
-            nameDB = "{}.tdb".format(params.get("DB_prefix", "TAMS"))
+        if params.get("database",{}).get("DB_save", False):
+            nameDB = "{}.tdb".format(params.get("database",{}).get("DB_prefix", "TAMS"))
             model = "pop_{}x{}x{}".format(
                 self._popDomain.get("Nx", 120),
                 self._popDomain.get("Ny", 56),
                 self._popDomain.get("Nz", 12),
             )
-            checkpoint_path = "{}/trajectories/{}".format(nameDB, ioprefix)
+            self.run_folder = "{}/trajectories/{}".format(nameDB, ioprefix)
             self.checkpoint_prefix = "{}/trajectories/{}/{}".format(
                 nameDB, ioprefix, model
             )
-            if not os.path.exists(checkpoint_path):
-                os.mkdir(checkpoint_path)
+            if not os.path.exists(self.run_folder):
+                os.mkdir(self.run_folder)
 
-    def advance(self, dt: float, forcingAmpl: float):
+    def advance(self, dt: float,
+                forcingAmpl: float):
         """Override the template."""
+        # On the first call to advance, initialize POP
         if self.pop is None:
             self.pop = getPOPinstance(
-                nml_file=self._nml_file,
-                nworkers=self._nProc_pop,
-                domain_dict=self._popDomain,
+                pop_domain_dict=self._popDomain,
+                pop_options=self._pop_params,
+                root_run_folder=self.run_folder,
             )
             if self.checkpoint_prefix:
                 setCheckPoint(self.pop, int(dt), self.checkpoint_prefix)
+            else:
+                disableCheckPoint(self.pop)
             if self._state is not None:
                 setRestart(self.pop, self._state)
-            print("advance with init. State is {}".format(self._state))
+            print("Start advancing with init. state {}".format(self._state))
 
-        tnow = self.pop.model_time
-        print(tnow.value_in(units.hour))
-        dt_s = dt * 1 | units.hour
-        tend = tnow + dt_s
-        self.pop.evolve_model(tend)
+        # Time stepping is month based, so exact length varies
+        tstart = self.pop.model_time
+        date = self.pop.get_model_date()
+        days_left = remainingdaysincurrentmonth(date)
+        year_length = daysincurrentyear(date)
+        tnow  = tstart
+        dt_d = days_left * 1 | units.day
+        tstoich_end = tstart + dt_d
 
-        # Retrieve the actual dt performed by POP
-        actual_dt = self.pop.model_time - tnow
+        print("Start stoich step: tstart = {}, t_end = {}".format(tstart.value_in(units.day), tstoich_end.value_in(units.day)))
+
+        # Set stoichastic amplitude
+        self._noise = np.random.randn(1)
+        sfwf_ampl = forcingAmpl * np.sqrt(dt_d.value_in(units.day)/year_length) * self._noise
+        setStoichForcingAmpl(self.pop,sfwf_ampl)
+
+        # Outer loop might not be necessary
+        # since we rely on POP internal sub-stepping
+        # but keep it for now
+        while tnow < tstoich_end:
+            tend = tnow + dt_d
+            self.pop.evolve_model(tend)
+            tnow = self.pop.model_time
+
+        # Retrieve the actual time for which POP was integrated
+        actual_dt = tnow - tstart
 
         # State is handled by a pointer to a check file
         self._state = getLastRestart(self.pop)
 
-        return actual_dt.value_in(units.hour)
+        # Convert to year
+        return actual_dt.value_in(units.day)/year_length
 
     def getCurState(self):
         """Override the template."""
@@ -77,19 +126,46 @@ class POPOmuseModel(ForwardModel):
         self._state = state
 
     def score(self) -> float:
-        """Override the template."""
-        tnow = self.pop.model_time
-        return tnow.value_in(units.hour) * np.random.rand(1).item() / 1000.0
+        """Normalized AMOC strength minus monthly reference."""
+        # When called, the date is the first of
+        # month following the month of interest
+        date = self.pop.get_model_date()
+        score_ref = monthly_reference(date)
+        score = (26.0 - self.pop.get_amoc_strength())/26.0
+        return score - score_ref
+
+    def noise(self):
+        """Return last generated noise."""
+        return self._noise
 
     @classmethod
     def name(self):
         """Return the model name."""
         return "POPOmuseModel"
 
+    def listProcess(self):
+        """List process currently running."""
+        for p in psu.process_iter():
+            if 'pop_worker' in p.name():
+                print(p, flush=True)
+
+    def killSleepingProcess(self):
+        """Kill pop process currently sleeping."""
+        for p in psu.process_iter():
+            if 'pop_worker' in p.name() and 'sleep' in p.status():
+                os.kill(p.pid(), signal.SIGKILL)
+
     def clear(self):
         """Clear any model internals."""
-        self.pop.cleanup_code()
-        self.pop.clear()
-        del self.pop
-        self.pop = None
+        print("Clearing model internals", flush=True)
+        time.sleep(2.0)
+        if self.pop:
+            self.pop.cleanup_code()
+            self.pop.clear()
+            del self.pop
+            self.pop = None
         self._state = None
+        #print("Process after del()", flush=True)
+        #self.listProcess()
+        time.sleep(2.0)
+        #self.killSleepingProcess()
